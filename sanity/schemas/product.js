fe53b434c120cc481bd9fb0577a6cacb93d2dcbe
ZiedAllouen <allouen.mohamed@EPISOUSSE.COM.TN>
2022-11-12 21:45:09 +0100

    wsch1

--- sanity/schemas/product.js
+++ sanity/schemas/product.js
@@ -11,6 +11,17 @@ export default {
             options:{
                 hotspot: true,
             }
-        }
+        },
+        {
+            name:'name',
+            title:'Name',
+            type:'string',
+        },
+        {
+            name:'slug',
+            title:'Slug',
+            type:'slug',
+        },
+
     ]
 }
